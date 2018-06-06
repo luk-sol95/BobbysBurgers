975f3e51c5489b83133c0997dd214f0660decb8f
Łukasz Sołtysiński <luk.sol95@gmail.com>
2018-06-06 18:26:39 +0200

    first commit

--- src/com/soltys/Main.java
+++ src/com/soltys/Main.java
@@ -7,22 +7,20 @@ public class Main {
 //        tomBurger.addAddition("Lettuce", 0.55);
 //        tomBurger.addAddition("Bacon", 1.25);
 //        tomBurger.addAddition("Pickle", 0.84);
-//        tomBurger.addAddition("Human Flesh", 9.99);
 //        tomBurger.addAddition("Avocado", 0.48);
-//        tomBurger.addAddition("asder", 2);
+//        tomBurger.addAddition("Carrot", 2);
 //        tomBurger.printDetails();
 
 //        HealthyBurger jerryBurger = new HealthyBurger(6, "Jerry", "Falafel");
 //        jerryBurger.addAddition("Lettuce", 0.75);
 //        jerryBurger.addAddition("Bacon", 1.30);
 //        jerryBurger.addAddition("Pickle", 0.84);
-//        jerryBurger.addAddition("Human Flesh", 9.99);
 //        jerryBurger.addAddition("Avocado", 0.48);
 //        jerryBurger.addAddition("Carrot", 0.44);
 //        jerryBurger.addAddition("Chocolate", 0.68);
 //        jerryBurger.printDetails();
 
-        DeluxeBurger soltysBurger = new DeluxeBurger(7.0, "Soltys", "WhiteSupreme", "Human Flesh");
+        DeluxeBurger soltysBurger = new DeluxeBurger(7.0, "Soltys", "White", "Bacon");
         soltysBurger.addAddition("Bacon", 0.41);
         soltysBurger.printDetails();
     }
